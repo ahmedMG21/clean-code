bc96c272c38056379d9d985e6ace5cdfbcf6369e
Fahad <falfarsi22@gmail.com>
2023-11-06 16:49:46 +0300

    change all variables name from data to sensor data (fahad)

--- assignment2.java
+++ assignment2.java
@@ -2,13 +2,13 @@ public
 class SensorDataProcessor {
   // Sensonr data and limits.
  public
-  double[][][] data;
+  double[][][]  sensorData;  //change all variables name from data to sensorData to make it clear and easy to detect
  public
   double[][] limit;
   // constructor
  public
-  DataProcessor(double[][][] data, double[][] limit) {
-    this.data = data;
+  DataProcessor(double[][][]  sensorData, double[][] limit) {
+    this. =  sensorData;
     this.limit = limit;
   }
   // Calculates the average of an array
@@ -27,22 +27,22 @@ class SensorDataProcessor {
     int j = 0;
     int k = 0;
     double[][][] data2 =
-        new double[data.length][data[0].length][data[0][0].length];
+        new double[ sensorData.length][ sensorData[0].length][ sensorData[0][0].length];
     BufferedWriter out;
     // Write racing stats data into a file
     try {
       out = new BufferedWriter(new FileWriter("RacingStatsData.txt"));
-      for (i = 0; i < data.length; i++) {
-        for (j = 0; j < data[0].length; j++) {
-          for (k = 0; k < data[0][0].length; k++) {
-            data2[i][j][k] = data[i][j][k] / divisor - Math.pow(limit[i][j], 2.0);
+      for (i = 0; i <  sensorData.length; i++) {
+        for (j = 0; j <  sensorData[0].length; j++) {
+          for (k = 0; k <  sensorData[0][0].length; k++) {
+            data2[i][j][k] =  sensorData[i][j][k] / divisor - Math.pow(limit[i][j], 2.0);
             if (average(data2[i][j]) > 10 && average(data2[i][j]) < 50)
               break;
-            else if (Math.max(data[i][j][k], data2[i][j][k]) > data[i][j][k])
+            else if (Math.max( sensorData[i][j][k], data2[i][j][k]) >  sensorData[i][j][k])
               break;
-            else if (Math.pow(Math.abs(data[i][j][k]), 3) <
+            else if (Math.pow(Math.abs( sensorData[i][j][k]), 3) <
                          Math.pow(Math.abs(data2[i][j][k]), 3) &&
-                     average(data[i][j]) < data2[i][j][k] &&
+                     average( sensorData[i][j]) < data2[i][j][k] &&
                      (i + 1) * (j + 1) > 0)
               data2[i][j][k] *= 2;
             else
